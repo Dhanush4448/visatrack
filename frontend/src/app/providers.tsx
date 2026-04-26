054fda2e645f5c32bcaf0caa8e4fd2150be0ec79
Dhanush4448 <dhanushn.cse2020@citchennai.net>
2026-04-26 12:10:49 -0400

    feat: add PostHog analytics

---
+++ frontend/src/app/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    posthog.init("phc_vXWiY8SrfwLEeyS74fvJX4fTR8ZHMn6fqqkLMzGSBu7m", {
+      api_host: "https://us.i.posthog.com",
+      capture_pageview: true,
+      capture_pageleave: true,
+    });
+  }, []);
+
+  return <PHProvider client={posthog}>{children}</PHProvider>;
+}
